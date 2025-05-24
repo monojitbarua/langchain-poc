37c4e1e5d94656a6310e352fd2b1ac9b6535193c
monojit-barua <monojit.barua@dell.com>
2025-05-24 07:49:43 +0000

    first commit

---
+++ 1-openai.py
@@ -0,0 +1,26 @@
+import json
+import os
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI
+
+# Load environment variables from .env file
+load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), ".env"))
+
+if __name__ == '__main__':
+    print("Hello langchain")
+    print(os.environ['OPENAI_API_KEY'])
+    summary_template = '''
+        Given the information "{data}" about this dataset, please provide a summary.
+    '''
+    prompt_template = PromptTemplate(input_variables=["data"], template=summary_template)
+
+    llm = ChatOpenAI(model_name="gpt-4.1-mini", temperature=0)
+
+    chain = prompt_template | llm
+
+    res = chain.invoke({"data": "Dell Technologies"})
+
+    print(res)
+    print()
+    print(json.dumps({"response": res.content}, indent=2))
---
+++ 2-ollama.py
@@ -0,0 +1,26 @@
+import json
+import os
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_ollama import ChatOllama
+from langchain_openai import ChatOpenAI
+from langchain_core.output_parsers import StrOutputParser
+
+# Load environment variables from .env file
+load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), ".env"))
+
+if __name__ == '__main__':
+    print("Hello langchain")
+    summary_template = '''
+        You are an AI assistant. Summarize the following dataset information:
+        "{data}".
+    '''
+    prompt_template = PromptTemplate(input_variables=["data"], template=summary_template)
+
+    llm = ChatOllama(model="llama3")
+
+    chain = prompt_template | llm | StrOutputParser()
+
+    res = chain.invoke({"data": "Dell Technologies"})
+
+    print(res)
